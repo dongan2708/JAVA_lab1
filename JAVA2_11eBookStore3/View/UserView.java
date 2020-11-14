459b7ebe0d7b40f24bc2bb280c4ed94c1a468303
dongan2708 <65014848+dongan2708@users.noreply.github.com>
2020-11-14 21:13:41 +0700

    Java02_09

---
+++ JAVA2_11eBookStore3/View/UserView.java
@@ -0,0 +1,41 @@
+package JAVA2_11eBookStore3.View;
+
+import JAVA2_11eBookStore3.Controllers.RegisterController;
+import JAVA2_11eBookStore3.Models.User;
+
+
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class UserView {
+    Scanner input = new Scanner(System.in);
+    public  String slt;
+    public  ResultSet rst;
+    BookShop bookShop = new BookShop();
+
+    public int checkUser(User user) {
+        int y = user.getRole();
+        if (y == 1) {
+            bookShop.menuCustomer(user);
+            return 1;
+        } else if (y == 2) {
+            bookShop.menuAdmin(user);
+            return 2;
+        }
+        return 0;
+    }
+
+    public void createAccount() {
+        System.out.println("Enter your account:");
+        String name = input.nextLine();
+        System.out.println("Enter your password:");
+        String password = input.nextLine();
+        System.out.println("Enter your role: (1-customer, 2-admin)");
+        int role = Integer.parseInt(input.nextLine());
+        User obj = new User(name, password, role);
+        RegisterController signUp = new RegisterController();
+        signUp.signUp(obj);
+        bookShop.menuUser();
+    }
+}
+
